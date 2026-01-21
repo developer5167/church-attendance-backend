0e82bc2c354214a59e0dedf4bc69c458d74afaf0
kruparaoKapil <krupagarao.g@kapilit.com>
2026-01-21 16:14:41 +0530

    skj

--- src/server.js
+++ src/server.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 4000;
 app.get("/", (req, res) => {
   res.send({"status":"ok" , "message":"Church Attendance API is running", "version":"1.0.0"});
 });
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
